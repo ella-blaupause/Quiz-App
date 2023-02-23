9d62c81bf9e3c73411c757b6f7a70b82025c5587
Ella Kempel <ella.lernt@mail.de>
2023-02-23 18:45:47 +0100

    test vercel

--- answer-button.js
+++ answer-button.js
@@ -4,6 +4,7 @@ const answerButton = document.querySelector('[data-js="answer-button"]');
 const answer = document.querySelector('[data-js="answer"]');
 
 answerButton.addEventListener("click", () => {
+  console.log("ich bin ein test");
   if (answerButton.innerText === "Antwort") {
     answerButton.innerText = "Antwort verbergen";
     answer.hidden = false;
