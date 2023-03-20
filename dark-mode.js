4c0c03c0007b28807f97d4bfd69861a88fc3d842
Stephan S. Hepper <s.hepper@netzkolchose.de>
2023-03-20 23:43:57 +0100

    Make darkmode switch work for all sites

--- dark-mode.js
+++ dark-mode.js
@@ -1,9 +1,29 @@
-console.clear();
+const DARK_MODE_STORE = 'dark_mode';
 
-const bodyElement = document.querySelector('[data-js="body"]');
+const bodyElement = document.querySelector('body');
 const toggleDark = document.querySelector('[data-js="toggle-dark"]');
 
-toggleDark.addEventListener("click", () => {
-  toggleDark.classList.toggle("switch-button--dark");
-  bodyElement.classList.toggle("dark");
-});
+const set_darkmode = (new_value) => {
+  localStorage.setItem(DARK_MODE_STORE, new_value);
+  if (!new_value) {
+    toggleDark?.classList.remove("switch-button--dark");
+    bodyElement?.classList.remove("dark");
+  } else {
+    toggleDark?.classList.add("switch-button--dark");
+    bodyElement?.classList.add("dark");
+  }
+}
+
+const get_darkmode = () => {
+  return JSON.parse(localStorage.getItem(DARK_MODE_STORE)) ?? false;
+}
+
+const toggle_darkmode = () => {
+  const dark_mode = get_darkmode();
+  set_darkmode(!dark_mode);
+}
+
+toggleDark?.addEventListener("click", toggle_darkmode);
+
+
+set_darkmode(get_darkmode());
